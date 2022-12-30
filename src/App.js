ac9031a6f9a82903be018f468afa6fb8a4b236e6
Son Bui <buisynamson@gmail.com>
2022-12-30 15:57:50 +0700

    done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./BaiTapLayoutComponent/Home"
+import './App.css';
+import Home from "./BaiTapLayoutComponent/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
